5d05f7f848468b8e44223bd7bae4bcd0921034a4
chaikuaizhang <chaikuaizhang@aliyun.com>
2016-01-10 22:33:57 +0800

    modify

--- src/tests/detailed/ui/WeixinGroupSelectPanel.java
+++ src/tests/detailed/ui/WeixinGroupSelectPanel.java
@@ -1,8 +1,12 @@
 package tests.detailed.ui;
 
+import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,7 +19,9 @@ import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
+import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import org.cef.browser.CefBrowser;
@@ -26,41 +32,61 @@ import tests.detailed.DomVisitor;
 public class WeixinGroupSelectPanel extends JPanel {
 
 	private List<String> selectedWeixinGroup = new ArrayList<String>();
-	private Map<String, JCheckBox> weixinUserMap = new HashMap<>();
+	private List<Map<String, JCheckBox>> weixinUserList = new ArrayList<>();
 	private final CefBrowser browser_;
-	private final DomVisitor domVisitor;	
+	private final DomVisitor domVisitor;
 	private static int count = 0;
 	private WeixinGroupControlPanel wgcp;
+	
 	public WeixinGroupSelectPanel(CefBrowser browser) {
 		domVisitor = new DomVisitor(this);
 		browser_ = browser;
-		setLayout(new GridLayout(0,6));
+		setLayout(new GridLayout(0, 6));
 	}
 
-	public void addCheckBox(Map<String, String> dataMap) {
+	public void addCheckBox(List<Map<String, String>> dataList) {
 		boolean hasData = false;
-		for(Map.Entry<String, String> entry : dataMap.entrySet()) {
-			if(!weixinUserMap.containsKey(entry.getKey())) {
-				JCheckBox box = new JCheckBox(entry.getValue());
+		for (Map<String, String> map : dataList) {
+			boolean hasExisted = false;
+			for(Map<String, JCheckBox> userMap : weixinUserList) {
+				if(userMap.containsKey(map.get("userName"))) {
+					hasExisted = true;
+					break;
+				}
+			}
+			if (!hasExisted) {
+				JCheckBox box = new JCheckBox(map.get("userNickName"));
 				add(box);
-				weixinUserMap.put(entry.getKey(), box);
+				Map<String, JCheckBox> boxMap = new HashMap<>();
+				boxMap.put(map.get("userName"), box);
+				weixinUserList.add(boxMap);
 				hasData = true;
 			}
 			count++;
 		}
-		if(hasData) {
+		if (hasData) {
 			count += 3 * 64;
-			browser_.executeJavaScript("$('#J_NavChatScrollBody')[0].scrollTop = "+ String.valueOf((count)), "", 9999);
-			Timer timer = new Timer();  
-		    timer.schedule(new TimerTask() {  
-		        public void run() {  
-		        	updatePanel(browser_);
-		        }  
-		    }, 2000);
+			browser_.executeJavaScript("$('#J_NavChatScrollBody')[0].scrollTop = " + String.valueOf((count)), "", 9999);
+			Timer timer = new Timer();
+			timer.schedule(new TimerTask() {
+				public void run() {
+					updatePanel(browser_);
+				}
+			}, 2000);
 		} else {
 			System.out.println("================>All End");
-			wgcp.textFiled_.setText("已完成加载");
+			browser_.executeJavaScript("$('#J_NavChatScrollBody')[0].scrollTop = " + String.valueOf((0)), "", 9999);
+			wgcp.textFiled_.setBackground(Color.GREEN);
+			wgcp.textFiled_.setText("已完成加载！");
 		}
+	}
+
+	public List<Map<String, JCheckBox>> getWeixinUserList() {
+		return weixinUserList;
+	}
+
+	public void setWeixinUserList(List<Map<String, JCheckBox>> weixinUserList) {
+		this.weixinUserList = weixinUserList;
 	}
 
 	public void updatePanel(CefBrowser browser) {
@@ -71,5 +97,9 @@ public class WeixinGroupSelectPanel extends JPanel {
 
 	public void setGroupControlPane(WeixinGroupControlPanel wgcp) {
 		this.wgcp = wgcp;
+	}
+	
+	public static void main(String[] args) {
+		
 	}
 }
